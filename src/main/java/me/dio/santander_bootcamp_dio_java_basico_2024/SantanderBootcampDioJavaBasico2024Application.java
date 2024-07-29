d3e908e7672a94edac5e5bf7ee32d6b1778d3c13
adriana <dircastorm@gmail.com>
2024-07-28 21:49:10 -0300

    first commit

---
+++ src/main/java/me/dio/santander_bootcamp_dio_java_basico_2024/SantanderBootcampDioJavaBasico2024Application.java
@@ -0,0 +1,13 @@
+package me.dio.santander_bootcamp_dio_java_basico_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderBootcampDioJavaBasico2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderBootcampDioJavaBasico2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/santander_bootcamp_dio_java_basico_2024/SantanderBootcampDioJavaBasico2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santander_bootcamp_dio_java_basico_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderBootcampDioJavaBasico2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
